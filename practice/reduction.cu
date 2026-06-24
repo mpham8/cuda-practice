51693317a5f29d2d8f57406a5b24e8066a4c4758
Michael Pham <pham_michael@outlook.com>
2026-06-24 13:57:12 -0400

    fix comments

--- practice/reduction.cu
+++ practice/reduction.cu
@@ -1,15 +1,8 @@
 #include <cuda_runtime.h>
 
 #define LOADS_PER_THREAD 4
-#define threadsPerBlock 256 //needs multiple 32
-#define SHARED_MEM_SIZE (threadsPerBlock/32)
-
-__device__ float warpReduce(float val){
-    #pragma unroll
-    for (int i = 16; i > 0; i>>=1) val += __shfl_down_sync(0xffffffff, val, i);
-    return val;
-    }
-
+#define THREADS_PER_BLOCK 256 //needs multiple of 32
+#define SHARED_MEM_SIZE (THREADS_PER_BLOCK/32)
 
 __global__ void reduction(const float* input, float* output, int N){
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -19,31 +12,29 @@ __global__ void reduction(const float* input, float* output, int N){
     
     __shared__ float data_s[SHARED_MEM_SIZE];
     
-    //LOAD MEM FROM GLOBAL MEM
+    //LOAD MEM FROM GLOBAL MEM, STRIDED
     float sum = 0.0f;
     #pragma unroll
     for (int i = idx; i < N; i += blockDim.x * gridDim.x) sum += input[i];
-    // data_s[tIdx] = sum;
-    // __syncthreads();
 
-    
-    //REDUCE W/IN BLOCK
-    // for (int i = blockDim.x; i > 32; i >>= 1){
-    //     if (tIdx < i) data_s[tIdx] += data_s[tIdx + i];
-    //     __syncthreads();
-    // }
-    
-    //warp reduction 256->8
-    sum = warpReduce(sum);
+    //W/IN BLOCK REDUCTION
+    //warp reduction (256 -> 8 per block)
+    #pragma unroll
+    for (int i = 16; i > 0; i>>=1) sum += __shfl_down_sync(0xffffffff, sum, i);
 
+    //save warp reduction results to shared mem, lane 0 only
     if (laneIdx == 0) data_s[warpIdx] = sum;
     __syncthreads();
 
-    //REDUCE TO 1 PER BLOCK AND GLOBALLY AMONG BLOCKS
+    //REDUCTION TO 1 PER BLOCK AND GLOBAL BLOCK REDUCTION
+    //warp 0 executes block reduction
     if (warpIdx == 0) {
         sum = (laneIdx < SHARED_MEM_SIZE) ? data_s[laneIdx] : 0.0f;
-        sum = warpReduce(sum);
-        //atomic add
+        
+        //warp reduction (8->1 per block)
+        #pragma unroll
+        for (int i = 16; i > 0; i>>=1) sum += __shfl_down_sync(0xffffffff, sum, i);
+        //atomic add, lane 0 only (blocksPerGrid -> 1 globally)
         if (laneIdx == 0) atomicAdd(output, sum);
     }
 
@@ -51,9 +42,9 @@ __global__ void reduction(const float* input, float* output, int N){
 
 // input, output are device pointers
 extern "C" void solve(const float* input, float* output, int N) {
-    // int threadsPerBlock = 256;
-    int loadsPerBlock = LOADS_PER_THREAD * threadsPerBlock; 
+    // int THREADS_PER_BLOCK = 256;
+    int loadsPerBlock = LOADS_PER_THREAD * THREADS_PER_BLOCK; 
     int blocksPerGrid = ( N + loadsPerBlock - 1 )/loadsPerBlock;
 
-    reduction<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    reduction<<<blocksPerGrid, THREADS_PER_BLOCK>>>(input, output, N);
 }
